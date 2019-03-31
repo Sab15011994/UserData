344488afa119510156bdff9e91e21948ee4e9898
Sab15011994 <shahsaurabh769@gmail.com>
2019-03-31 15:39:02 +0530

    first commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from  '@angular/common/http';
+import  { Observable }  from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  
+  constructor(private http:HttpClient) { }
+
+  getUsers(): Observable<any>{
+
+    return this.http.get('https://jsonplaceholder.typicode.com/users')
+    
+  }
+  getUser(userId){
+
+    return this.http.get('https://jsonplaceholder.typicode.com/users/'+userId)
+    
+  }
+
+  getPosts(){
+
+    return this.http.get('https://jsonplaceholder.typicode.com/posts')
+    
+  }
+
+  }
+
